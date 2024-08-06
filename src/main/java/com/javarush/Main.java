d1a643582f1b129de2fda56344dd168898115849
EvaIablocova <125370412+EvaIablocova@users.noreply.github.com>
2024-08-06 14:34:04 +0300

    logic

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -1,12 +1,19 @@
 package com.javarush;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import com.javarush.domain.*;
 import com.javarush.dao.*;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.Year;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 
 public class Main {
 
@@ -77,6 +84,127 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
+//        Customer customer = main.createCustomer();
+//        main.customerReturnInventoryToStore();
+//        main.customerRentInventory(customer);
+
+        main.newFilmWasMade();
 
     }
+
+    private void newFilmWasMade(){
+        try(Session session = sessionFactory.getCurrentSession()){
+            session.beginTransaction();
+
+            Language language =languageDAO.getItems(0, 20).stream().unordered().findAny().get();
+            List<Category> categories = categoryDAO.getItems(0, 5);
+            List<Actor> actors = actorDAO.getItems(0, 15);
+
+            Set<Feature> features = new HashSet<>();
+            features.add(Feature.TRAILERS);
+            features.add(Feature.BEHIND_THE_SCENES);
+
+
+            Film film = new Film();
+            film.setLanguage(language);
+            film.setActors(new HashSet<>(actors));
+            film.setCategories((new HashSet<>(categories)));
+            film.setRating(Rating.PG);
+            film.setSpecialFeatures(features);
+            film.setReplacementCost(BigDecimal.TEN);
+            film.setRentalRate(BigDecimal.ZERO);
+            film.setDescription("new scary film");
+            film.setTitle("scary my movie");
+            film.setRentalDuration((byte)44);
+            film.setOriginalLanguage(language);
+            film.setYear(Year.now());
+            filmDAO.save(film);
+
+            FilmText filmText = new FilmText();
+            filmText.setFilm(film);
+            filmText.setDescription(film.getDescription());
+            filmText.setTitle(film.getTitle());
+            filmText.setId(film.getId());
+            filmTextDAO.save(filmText);
+
+            session.getTransaction().commit();
+        }
+    }
+
+
+    private void customerReturnInventoryToStore() {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+
+            Rental rental = rentalDAO.getAnyUnreturnedRental();
+            rental.setReturnDate(LocalDateTime.now());
+
+            rentalDAO.save(rental);
+
+            session.getTransaction().commit();
+        }
+    }
+
+    private Customer createCustomer(){
+        try(Session session = sessionFactory.getCurrentSession()){
+            session.beginTransaction();
+            Store store = storeDAO.getItems(0,1).get(0);
+
+            City city = cityDAO.getByName("Chisinau");
+
+            Address address = new Address();
+            address.setAddress("Indep str, 48");
+            address.setPhone("999-111-555");
+            address.setCity(city);
+            address.setDistrict("Assasas");
+            addressDAO.save(address);
+
+            Customer customer = new Customer();
+            customer.setAddress(address);
+            customer.setEmail("test@gmail.com");
+            customer.setActive(true);
+            customer.setStore(store);
+            customer.setFirstName("Vasya");
+            customer.setLastName("Pupkin");
+            customerDAO.save(customer);
+
+            session.getTransaction().commit();
+            return customer;
+        }
+    }
+
+    private void customerRentInventory(Customer customer){
+        try(Session session = sessionFactory.getCurrentSession()){
+            session.beginTransaction();
+
+            Film film = filmDAO.getFirstAvalableFilmForRent();
+            Store store = storeDAO.getItems(0,1).get(0);
+
+            Inventory inventory = new Inventory();
+            inventory.setFilm(film);
+            inventory.setStore(store);
+            inventoryDAO.save(inventory);
+
+            Staff staff = store.getStaff();
+
+            Rental rental = new Rental();
+            rental.setCustomer(customer);
+            rental.setRentalDate(LocalDateTime.now());
+            rental.setInventory(inventory);
+            rental.setStaff(staff);
+            rentalDAO.save(rental);
+
+            Payment payment = new Payment();
+            payment.setCustomer(customer);
+            payment.setRental(rental);
+            payment.setPaymentDate(LocalDateTime.now());
+            payment.setAmount(BigDecimal.valueOf(55.47));
+            payment.setStaff(staff);
+            paymentDAO.save(payment);
+
+            session.getTransaction().commit();
+        }
+    }
+
+
 }
--- src/main/java/com/javarush/domain/FilmText.java
+++ src/main/java/com/javarush/domain/FilmText.java
@@ -20,4 +20,36 @@ public class FilmText {
     @Column(columnDefinition = "text")
     @Type(type = "text")
     private String description;
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
